ChristopherPH/KeyboardTester

commit ef73be59f79f76239147ed9b95c59f6636ff837f

feat: Split skip fake shift keys as an independant option

FormMain+KeyboardHandler.cs
@@ -10,27 +10,38 @@ namespace KeyboardTester
         const int WM_SYSKEYDOWN = 0x104;
         const int WM_SYSKEYUP = 0x105;
 
-        public RawInput rawInput;
-        public uint skipShiftUp = 0;
-        public uint skipShiftDown = 0;
-        public uint keysInFakeShift = 0;
+        private RawInput rawInput;
+        private uint skipShiftUp = 0;
+        private uint skipShiftDown = 0;
+        private uint keysInFakeShift = 0;
 
-        enum NumPadNumlockModes
+        /// <summary>
+        /// Numeric Keypad adjustment modes:
+        /// When NumLock is ON, shift is held down, and a numeric keypad key is pressed
+        /// </summary>
+        enum NumberPadAdjustmentModes
         {
-            Default,
-            ShiftedNumberKey,
-            IgnoreShift,
+            /// <summary>
+            /// Unshift numeric keypad key (This is standard Windows behaviour)
+            /// Treat as if numlock is off and shift is not held down
+            /// </summary>
+            Unshift,
+
+            /// <summary>
+            /// Shift the numeric keypad key
+            /// </summary>
+            Shift,
+
+            /// <summary>
+            /// Ignore the shift key
+            /// </summary>
+            Ignore,
         }
 
-        NumPadNumlockModes NumlockModes
-        {
-            get => _numlockModes;
-            set
-            {
-                _numlockModes = value;
-            }
-        }
-        NumPadNumlockModes _numlockModes = NumPadNumlockModes.Default;
+        bool SkipFakeShiftKeyPresses { get; set; } = false;
+
+
+        NumberPadAdjustmentModes NumlockMode { get; set; } = NumberPadAdjustmentModes.Unshift;
 
         private void InitKeyboardHandler()
         {
@@ -48,9 +59,6 @@ namespace KeyboardTester
         {
             //System.Diagnostics.Debug.Print($"RAW {(e.KeyState == RawInputKeyStates.Down ? "Pressed" : "Released")} {e.Key}");
 
-            if (NumlockModes == NumPadNumlockModes.Default)
-                return;
-
             /* Key is shift, but scancode is not left or right shift
              * (Scancode will be that of key that generated fake press)
              * https://learn.microsoft.com/en-us/windows/win32/inputdev/about-keyboard-input
@@ -61,12 +69,16 @@ namespace KeyboardTester
             {
                 if (e.KeyState == RawInputKeyStates.Down)
                 {
-                    skipShiftDown++;
+                    if (SkipFakeShiftKeyPresses)
+                        skipShiftDown++;
+
                     keysInFakeShift--;
                 }
                 else
                 {
-                    skipShiftUp++;
+                    if (SkipFakeShiftKeyPresses)
+                        skipShiftUp++;
+
                     keysInFakeShift++;
                 }
             }
@@ -100,9 +112,8 @@ namespace KeyboardTester
                     }
 
                     //Adjust number pad keys if within a fake shift block
-                    if (keysInFakeShift > 0)
-                        keyData = AdjustNumberPadKeyData(msg, keyData,
-                            NumlockModes == NumPadNumlockModes.IgnoreShift);
+                    keyData = AdjustNumberPadKeyData(msg, keyData,
+                        keysInFakeShift > 0, NumlockMode);
 
                     return HandleKeyPress(keyData, true);
 
@@ -118,9 +129,8 @@ namespace KeyboardTester
                     }
 
                     //Adjust number pad keys if within a fake shift block
-                    if (keysInFakeShift > 0)
-                        keyData = AdjustNumberPadKeyData(msg, keyData,
-                            NumlockModes == NumPadNumlockModes.IgnoreShift);
+                    keyData = AdjustNumberPadKeyData(msg, keyData,
+                        keysInFakeShift > 0, NumlockMode);
 
                     return HandleKeyPress(keyData, false);
             }
@@ -128,8 +138,12 @@ namespace KeyboardTester
             return false;
         }
 
-        private Keys AdjustNumberPadKeyData(Message msg, Keys keyData, bool SkipShift = false)
+        private static Keys AdjustNumberPadKeyData(Message msg, Keys keyData,
+            bool inFakeShift, NumberPadAdjustmentModes numlockMode)
         {
+            if (numlockMode == NumberPadAdjustmentModes.Unshift)
+                return keyData;
+
             //Split keycode from modifiers
             var keyCode = keyData & Keys.KeyCode;
             var modifiers = keyData & Keys.Modifiers;
@@ -158,7 +172,7 @@ namespace KeyboardTester
                 }
 
                 //Add shift modifier to the key
-                if (!SkipShift)
+                if (numlockMode == NumberPadAdjustmentModes.Shift)
                     modifiers |= Keys.Shift;
 
                 //Rebuild key press

FormMain.Designer.cs
@@ -40,6 +40,7 @@
             this.rbNumLockShifted = new System.Windows.Forms.RadioButton();
             this.rbNumLockDefault = new System.Windows.Forms.RadioButton();
             this.txtLog = new System.Windows.Forms.TextBox();
+            this.chkIgnoreFakeShift = new System.Windows.Forms.CheckBox();
             this.gbNumLock.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -51,7 +52,7 @@
             this.lblInstructions.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.lblInstructions.Location = new System.Drawing.Point(12, 9);
             this.lblInstructions.Name = "lblInstructions";
-            this.lblInstructions.Size = new System.Drawing.Size(598, 293);
+            this.lblInstructions.Size = new System.Drawing.Size(598, 337);
             this.lblInstructions.TabIndex = 0;
             this.lblInstructions.Text = "Instructions:";
             // 
@@ -68,7 +69,7 @@
             this.lvPressedKeys.FullRowSelect = true;
             this.lvPressedKeys.GridLines = true;
             this.lvPressedKeys.HideSelection = false;
-            this.lvPressedKeys.Location = new System.Drawing.Point(12, 305);
+            this.lvPressedKeys.Location = new System.Drawing.Point(12, 349);
             this.lvPressedKeys.MultiSelect = false;
             this.lvPressedKeys.Name = "lvPressedKeys";
             this.lvPressedKeys.Size = new System.Drawing.Size(598, 281);
@@ -101,12 +102,13 @@
             // 
             this.gbNumLock.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbNumLock.Controls.Add(this.chkIgnoreFakeShift);
             this.gbNumLock.Controls.Add(this.rbNumLockIgnore);
             this.gbNumLock.Controls.Add(this.rbNumLockShifted);
             this.gbNumLock.Controls.Add(this.rbNumLockDefault);
             this.gbNumLock.Location = new System.Drawing.Point(616, 9);
             this.gbNumLock.Name = "gbNumLock";
-            this.gbNumLock.Size = new System.Drawing.Size(336, 290);
+            this.gbNumLock.Size = new System.Drawing.Size(336, 334);
             this.gbNumLock.TabIndex = 2;
             this.gbNumLock.TabStop = false;
             this.gbNumLock.Text = "Shift + Numeric Keypad Handling";
@@ -148,7 +150,7 @@
             // txtLog
             // 
             this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtLog.Location = new System.Drawing.Point(616, 305);
+            this.txtLog.Location = new System.Drawing.Point(616, 349);
             this.txtLog.Multiline = true;
             this.txtLog.Name = "txtLog";
             this.txtLog.ReadOnly = true;
@@ -156,11 +158,21 @@
             this.txtLog.Size = new System.Drawing.Size(336, 281);
             this.txtLog.TabIndex = 3;
             // 
+            // chkIgnoreFakeShift
+            // 
+            this.chkIgnoreFakeShift.Location = new System.Drawing.Point(17, 288);
+            this.chkIgnoreFakeShift.Name = "chkIgnoreFakeShift";
+            this.chkIgnoreFakeShift.Size = new System.Drawing.Size(300, 31);
+            this.chkIgnoreFakeShift.TabIndex = 6;
+            this.chkIgnoreFakeShift.Text = "Ignore fake shift keypresses";
+            this.chkIgnoreFakeShift.UseVisualStyleBackColor = true;
+            this.chkIgnoreFakeShift.CheckedChanged += new System.EventHandler(this.chkIgnoreFakeShift_CheckedChanged);
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(964, 598);
+            this.ClientSize = new System.Drawing.Size(964, 642);
             this.Controls.Add(this.txtLog);
             this.Controls.Add(this.gbNumLock);
             this.Controls.Add(this.lvPressedKeys);
@@ -189,6 +201,7 @@
         private System.Windows.Forms.RadioButton rbNumLockIgnore;
         private System.Windows.Forms.RadioButton rbNumLockShifted;
         private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.CheckBox chkIgnoreFakeShift;
     }
 }
 

FormMain.cs
@@ -24,6 +24,7 @@ namespace KeyboardTester
             rbNumLockDefault.Enabled = numLock;
             rbNumLockShifted.Enabled = numLock;
             rbNumLockIgnore.Enabled = numLock;
+            chkIgnoreFakeShift.Enabled = numLock;
 
             if (!numLock)
             {
@@ -180,7 +181,7 @@ Holding both shift keys and pressing a key on the numeric keypad will:
         {
             if ((sender is RadioButton rb) && (rb.Checked))
             {
-                NumlockModes = NumPadNumlockModes.Default;
+                NumlockMode = NumberPadAdjustmentModes.Unshift;
             }
         }
 
@@ -188,7 +189,7 @@ Holding both shift keys and pressing a key on the numeric keypad will:
         {
             if ((sender is RadioButton rb) && (rb.Checked))
             {
-                NumlockModes = NumPadNumlockModes.ShiftedNumberKey;
+                NumlockMode = NumberPadAdjustmentModes.Shift;
             }
         }
 
@@ -196,8 +197,14 @@ Holding both shift keys and pressing a key on the numeric keypad will:
         {
             if ((sender is RadioButton rb) && (rb.Checked))
             {
-                NumlockModes = NumPadNumlockModes.IgnoreShift;
+                NumlockMode = NumberPadAdjustmentModes.Ignore;
             }
+        }
+
+        private void chkIgnoreFakeShift_CheckedChanged(object sender, EventArgs e)
+        {
+            if (sender is CheckBox cb)
+                SkipFakeShiftKeyPresses = cb.Checked;
         }
     }
 }

commit 5b45d08373de8522784bb3baf81c1d005e93252e

fix: Fix issue where keys are adjusting when not in a fake shift
Introduced by ef73be59f79f76239147ed9b95c59f6636ff837f

FormMain+KeyboardHandler.cs
@@ -159,7 +159,9 @@ namespace KeyboardTester
         private static Keys AdjustNumberPadKeyData(Message msg, Keys keyData,
             bool numLock, bool inFakeShift, NumberPadAdjustmentModes adjustmentMode)
         {
-            if (!numLock || (adjustmentMode == NumberPadAdjustmentModes.Unshift))
+            //Only need to adjust when numlock is on, and in a fake shift,
+            //and adjustment mode is not the default.
+            if (!numLock || !inFakeShift || (adjustmentMode == NumberPadAdjustmentModes.Unshift))
                 return keyData;
 
             //Split keycode from modifiers